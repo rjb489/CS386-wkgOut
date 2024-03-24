8e321b7f7670fcc7362057bc941ac1124793c9b3
Charles Diaz <chd63@nau.edu>
2024-03-23 19:38:45 -0700

    edited the main.js to post to the connecting script, and edited connecting script to take that information and put it into sql

--- Frontend/SignUpPage/main.js
+++ Frontend/SignUpPage/main.js
@@ -1,3 +1,6 @@
+
+
+
 document.addEventListener("DOMContentLoaded", function(){
     let form = document.getElementById("userInfo");
 
@@ -9,7 +12,50 @@ document.addEventListener("DOMContentLoaded", function(){
         let password = document.getElementById("password").value;
         let confirmPassword = document.getElementById("confirmPassword").value;
 
-        //add the data to database here
+        if(password == confirmPassword)
+           {
+             // Send data as an object
+            const requestData = {
+               username: username,
+               password: password
+             };
+
+             //add the data to database here
+             sendData(requestData);
+           }
+
+    
+
     });
+
+
     console.log(username);
 });
+
+// make a post request to the server
+
+
+function sendData(userData) 
+{
+
+fetch('http://23.254.211.151:3000/createAccount',{
+
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(userData)
+})
+.then(response => {
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return response.json();
+})
+.then(data => {
+    console.log('Response from server:', data);
+})
+.catch(error => console.error('Error:', error));
+
+
+}
