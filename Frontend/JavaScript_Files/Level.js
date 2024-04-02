55c49593e679e8cf4d4db0049f969308fc2afe07
Riley Burke <rjb489@nau.edu>
2024-04-01 20:54:50 -0700

    Created D5 and started working on Level.js

---
+++ Frontend/JavaScript_Files/Level.js
@@ -0,0 +1,15 @@
+// Made by Riley Burke
+class Level
+   {
+    // the constructor
+    constructor(curLevel, curXP, totalXP)
+      {
+       this.curLevel = curLevel;
+       this.curXP = curXP;
+       this.totalXP = totalXP;
+      }
+
+
+    // needs to be able to update level when xp threshold
+    // is reached, and reset the curXP to the proper amount
+   }
